223c42dbce7ed3c19ebebd89a092207fa37adf3f
Sajad Shafi <maliksajad004@gmail.com>
2023-02-23 13:03:10 +0530

    fixed little issue

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -1,5 +1,6 @@
+import React from 'react';
 const Button = ({ text, classes }) => {
-return (
+  return (
     <button
       className={`px-4 py-2 bg-primary rounded shadow-md shadow-lightgray text-light dark:shadow-dark hover:bg-secondary ${classes}`}
       type="button">
